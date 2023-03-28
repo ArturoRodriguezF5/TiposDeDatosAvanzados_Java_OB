c7a47625380bf75cb62eb186cb7962cc2a12b3f7
Arturo Rodríguez Flores <114115632+ArturoRodriguezF5@users.noreply.github.com>
2023-03-28 17:04:29 -0600

    Primer commit sobre tipo de datos avanzados en Java.

---
+++ src/tiposdatos/avanzados/Strings.java
@@ -0,0 +1,12 @@
+package tiposdatos.avanzados;
+
+public class Strings {
+    public static void main(String[] args) {
+
+        String palabra = "Tamarindo";
+
+        for (int i = 0; i < palabra.length(); i++) {
+            System.out.println(palabra.charAt(i));
+        }
+    }
+}
